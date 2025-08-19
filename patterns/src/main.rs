6b65a2abd6ffe2802d6d469848d2b5d4b2261bd3
Baktash Gorgani <17418270+BaktashGorgani@users.noreply.github.com>
2025-08-19 00:04:16 -0400

    CH19 END

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -120,4 +120,48 @@ fn main() {
     match origin {
         Point2 { x, .. } => println!("x is {x}"),
     }
+
+    let num = Some(4);
+
+    match num {
+        Some(x) if x % 2 == 0 => println!("The number {x} is even"),
+        Some(x) => println!("The number is {x} is odd"),
+        None => (),
+    }
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {n}"),
+        _ => println!("Default case, x = {x:?}"),
+    }
+
+    println!("at the end: x = {x:?}, y = {y}");
+
+    let x = 4;
+    //let y = false;
+    let y = true;
+
+    match x {
+        4 | 5 | 6 if y => println!("yes"),
+        _ => println!("no"),
+    }
+
+    enum Message2 {
+        Hello { id: i32 },
+    }
+
+    let msg = Message2::Hello { id: 5 };
+
+    match msg {
+        Message2::Hello {
+            id: id_variable @ 3..=7,
+        } => println!("Found an id in range: {id_variable}"),
+        Message2::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        Message2::Hello { id } => println!("Found some other id: {id}"),
+    }
 }
